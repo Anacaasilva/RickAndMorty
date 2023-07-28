b35b5c2dc0aa15acfc6fc628637acc841476093b
Ana Carolina <anacarolina.oliveira1020@gmail.com>
2023-07-28 17:26:48 -0300

    cards layout

--- screens/characters/index.js
+++ screens/characters/index.js
@@ -1,11 +1,29 @@
 const list = document.getElementById('list');
 
-const showItens = data => {
+const showItens = ({ results }) => {
 
-  data.results.map(character => {
+  results.map(({ name, image, status }) => {
+
+    let color = '';
+
+    if (status == 'Alive') color = '#49C141';
+    else if (status == 'Dead') color = '#E40303';
+    else color = '#A1A6A1';
+
     list.innerHTML += `
-        <h1> ${character.name} </h1>
-        <img src=${character.image}>
+        <div class="card">
+        <div class="backgroundCard">
+          <img class="imgCharacter" src="${image}" alt="Image character">
+          <h2 class="nameCharacter">${name}</h2>
+          <div class="containerStatus">
+            <div class="circle" style="background-color: ${color} "></div>
+            <p class="status" style="color: ${color}">${status}</p>
+          </div>
+          <div class="containerButton">
+            <button class="button" type="button">Ver sobre</button>
+          </div>
+        </div>
+      </div> 
         `
   })
 
