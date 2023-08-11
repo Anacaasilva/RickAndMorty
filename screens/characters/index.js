798e3cda39783e23f951a941202627b791e48372
Ana Carolina <anacarolina.oliveira1020@gmail.com>
2023-08-11 17:31:48 -0300

    Create character modal

--- screens/characters/index.js
+++ screens/characters/index.js
@@ -1,8 +1,48 @@
 const list = document.getElementById('list');
+const containerModal = document.getElementById('containerModal');
+
+
+const requestCharacter = (id) => {
+  api(`https://rickandmortyapi.com/api/character/${id}`)
+    .then(data => showModal(data))
+    .catch(_err => showErr())
+
+}
+
+const showModal = (character) => {
+
+  let color = '';
+
+  if (character.status == 'Alive') color = '#49C141';
+  else if (character.status == 'Dead') color = '#E40303';
+  else color = '#A1A6A1';
+
+  containerModal.innerHTML = `
+    <div class="modal">
+      <div class="backgroundModal">
+        <div class="containerCharacter">
+          <h2 class="nameCharacter">${character.name}</h2>
+          <img class="imgModal" src="${character.image}" alt="Image character">
+        </div>
+        <div class="statusModal">
+          <div style="background-color:${color}"></div>
+          <p style="color:${color}">${character.status}</p>
+        </div>
+        <div class="containerInfo">
+          <p class="infoCharacter"><strong>Gender:</strong> ${character.gender}</p>
+          <p class="infoCharacter"><strong>Species:</strong> ${character.species}</p>
+          <p class="infoCharacter"><strong>Type:</strong> ${character.type}</p>
+          <p class="infoCharacter"><strong>Origin:</strong> ${character.origin.name}</p>
+          <p class="infoCharacter"><strong>Location:</strong> ${character.location.name}</p>
+        </div>
+      </div>
+    </div>
+  `
+}
 
 const showItens = ({ results }) => {
 
-  results.map(({ name, image, status }) => {
+  results.map(({ name, image, status, id }) => {
 
     let color = '';
 
@@ -20,7 +60,7 @@ const showItens = ({ results }) => {
             <p class="status" style="color: ${color}">${status}</p>
           </div>
           <div class="containerButton">
-            <button class="button" type="button">Ver sobre</button>
+            <button class="button" type="button" onclick=requestCharacter(${id})>Ver sobre</button>
           </div>
         </div>
       </div> 
